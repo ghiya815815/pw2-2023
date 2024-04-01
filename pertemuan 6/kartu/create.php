a5ea95038acf4fcba625dc8e2dafed086804b669
ghiyadhika <caghiya.dhk@gmail.com>
2024-04-01 13:07:58 +0700

    upload Tugas Kartu

--- pertemuan 6/kartu/create.php
+++ pertemuan 6/kartu/create.php
@@ -24,22 +24,33 @@ if(isset($_POST['tambah'])){
 <head>
     <meta charset="UTF-8">
     <title>Tambah Data</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Tambah Data</h1>
-    <form method="POST" action="">
+    <form class="form-horizontal w-75 mt-5 mx-auto p-4 border shadow-sm" method= "POST" action = "">
+        <div class="container">
+        <h1>Tambah Data</h1>
+    
         <label for="kode">Kode :</label>
         <input type="text" name="kode" required>
+    
         <br>
+    
         <label for="nama">Nama :</label>
         <input type="text" name="nama" required>
+    
         <br>
+    
         <label for="diskon">Diskon :</label>
         <input type="number" name="diskon" required>
+    
         <br>
+    
         <label for="iuran">Iuran :</label>
         <input type="number" name="iuran" required>
-        <input type="submit" name="tambah" value="Tambah">
+    
+        <input type="submit" name="tambah" class="btn btn-primary" action ="index.php" value="Tambah">
+        </div>
     </form>
 </body>
 </html>
--- pertemuan 6/kartu/index.php
+++ pertemuan 6/kartu/index.php
@@ -14,7 +14,7 @@ if(isset($_GET['id'])){
     if($Kartu->delete()){
         header("Location: index.php");
     }else{
-        echo "Gagal menghapus produk.";
+        echo "Gagal menghapus Kartu.";
     }
 }
 
@@ -26,11 +26,14 @@ $data = $Kartu->index();
 <head>
     <meta charset="UTF-8">
     <title>Daftar Kartu</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<form class="form-horizontal w-75 mt-5 mx-auto p-4 border shadow-sm" action = "">
+<div class="container">
     <h1>Daftar Kartu</h1>
     <a href="create.php">Tambah</a>
-    <table border="1">
+    <table class= "table" border="1">
         <thead>
             <tr>
                 <th>ID</th>
@@ -53,11 +56,13 @@ $data = $Kartu->index();
                     <td>
                         <a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a>
                         |
-                        <a href="index.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus produk ini?')">Hapus</a>
+                        <a href="index.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus Kartu ini?')">Hapus</a>
                     </td>
                 </tr>
             <?php } ?>
         </tbody>
     </table>
+    </div>
+    </form>
 </body>
 </html>
